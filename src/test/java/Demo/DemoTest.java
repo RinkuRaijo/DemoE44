31affcd144e15772b79e5bf2ea24f1d527b10413
RON <RON@LAPTOP-4OLM1RHQ>
2026-07-10 19:46:30 +0530

    committed

---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test(groups = "smoke")
+	
+	public void movieTest()
+	{
+		System.out.println("Toxic");
+	}
+
+}
---
+++ src/test/java/Demo/DemoTest1.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	
+	@Test(groups = "regression")
+	
+	public void movieTest()
+	{
+		System.out.println("Cocktail");
+	}
+
+}
